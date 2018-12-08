7db5501378c759df2b6cad4913876971245dc286
龙腾道 <LongTengDao@LongTengDao.com>
2018-12-08 21:34:19 +0800

    build

--- build/build.js
+++ build/build.js
@@ -3,14 +3,14 @@
 require('@ltd/j-dev').
 	
 	build({
-		dir: __dirname,
+		dir: __dirname+'/..',
 		ES: 3,
 		name: 'j-vue',
 		description: `
 			对 .vue 文件进行构建后的前端统一依赖。从属于“简计划”。
 			The unified dependency for built .vue files. Belong to "Plan J".`,
 		semver:
-			require('fs').readFileSync(__dirname+'/src/semver.json', 'utf8').match(/\d+/g).join('.'),
+			require('fs').readFileSync(__dirname+'/../src/semver.json', 'utf8').match(/\d+/g).join('.'),
 		UMD: {
 			ES: 5,
 			meta_name: 'jVue',
