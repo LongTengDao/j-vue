a1ffe3b6fb7ce17d677a2662e95511b447d5a83a
龙腾道 <LongTengDao@LongTengDao.com>
2018-12-24 07:14:21 +0800

    ...

--- build/build.js
+++ build/build.js
@@ -9,8 +9,8 @@ require('@ltd/j-dev')(__dirname+'/..')(async ({ build, read }) => {
 			对 .vue 文件进行构建后的前端统一依赖。从属于“简计划”。
 			The unified dependency for built .vue files. Belong to "Plan J".`,
 		semver: await read('src/version'),
-		ES: 3,
-		UMD: { ES: 5 },
+		ES: 5,
+		UMD: true,
 		DOC: true,
 	});
 	
