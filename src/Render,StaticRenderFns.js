e997ceca4ab8a23b1100e6f51efec7c0cdf12b54
龙腾道 <LongTengDao@LongTengDao.com>
2019-02-22 22:39:46 +0800

    v7.2.0

--- src/Render,StaticRenderFns.js
+++ src/Render,StaticRenderFns.js
@@ -1,22 +1,17 @@
 export function Render (code, scope) {
-	return RenderFn(scope ? code.replace(scope._search, scope._replacer) : code);
+	return Function(
+		'with(this){return '+( scope
+			? code.replace(scope._search, scope._replacer)
+			: code
+		)+'}'
+	);
 }
 
 export function StaticRenderFns (codes, scope) {
-	var fns = [];
-	var length = codes.length;
-	var index = 0;
-	if ( scope ) {
-		var _search = scope._search;
-		var _replacer = scope._replacer;
-		while ( index<length ) { fns.push(RenderFn(codes[index++].replace(_search, _replacer))); }
-	}
-	else {
-		while ( index<length ) { fns.push(RenderFn(codes[index++])); }
-	}
-	return fns;
-}
-
-function RenderFn (code) {
-	return Function('with(this){return '+code+'}');
+	return Function(
+		'return[function(){with(this){return '+( scope
+			? codes.join('}},function(){with(this){return ').replace(scope._search, scope._replacer)
+			: codes.join('}},function(){with(this){return ')
+		)+'}}]'
+	)();
 }
