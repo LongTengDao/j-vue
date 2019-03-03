11856510881845fefe6559713b2d5829aaba4ece
龙腾道 <LongTengDao@LongTengDao.com>
2019-03-03 17:40:02 +0800

    v8.1.0

--- dist/TSD/j-vue.d.ts
+++ dist/TSD/j-vue.d.ts
@@ -1,27 +1,29 @@
+type scope = object | ( (value :string | object | any[]) => string );
+
 declare module 'j-vue' {
 	
 	export const version :string;
 	
 	export function Identifier () :string;
 	
-	export function Scope () :Function;
 	export function Scope (keys :string) :object;
+	export function Scope () :(value :string | object | any[]) => string;
 	
-	export function Template (html: string, scope :Function | object) :string;
-	export function Render (code :string, scope? :Function | object) :Function;
-	export function StaticRenderFns (codes :string[], scope? :Function | object) :Function[];
+	export function Template (html :string, scope :scope) :string;
 	
-	export const STYLE :object;
+	export function Render (code :string, scope? :scope) :Function;
+	export function StaticRenderFns (codes :string[], scope? :scope) :Function[];
 	
-	export function Style (css? :string, scope? :Function | object) :HTMLStyleElement;
-	
+	export function Style (css? :string, scope? :scope) :HTMLStyleElement;
 	export function remove (style :HTMLStyleElement) :typeof remove;
+	
+	export const STYLE :{ functional :true, render :Function };
 	
 }
 
 declare module 'j-vue?*' {
 	export * from 'j-vue';
-	export const scope :Function | object;
+	export const scope :scope;
 	export const template :string;
 	export const render :Function;
 	export const staticRenderFns :Function[];
--- src/Scope.ts
+++ src/Scope.ts
@@ -1,36 +1,36 @@
 import { groupify } from '@ltd/j-groupify';
 import { Identifier } from './Identifier';
 
-var undefined :undefined;
 var isArray :(arg :any) => boolean = Array.isArray;
 var create :(o :object | null, properties?) => object = Object.create;
 var SEARCH :RegExp = /__[a-z][a-z0-9]*(?:_[a-z0-9]+)*__/ig;
+var KEYS :RegExp = /[a-z][a-z0-9]*(?:_[a-z0-9]+)*/ig;
 var NULL :object = Object.freeze(Object.create(null));
+var EMPTY :[] = [];
 ObjectScope.prototype = NULL;
 
-export default function Scope (this :object[] | object | any, keys? :string) :Function | object {
-	if ( keys===undefined ) { return FunctionScope(isArray(this) ? mix(this) : create(this instanceof ObjectScope ? this : null)); }
-	var proto :object;
-	if ( isArray(this) ) { proto = mix(this); }
-	else if ( this instanceof ObjectScope ) { proto = this; }
-	else { return new ObjectScope(keys.split('|')); }
-	InheritedObjectScope.prototype = proto;
-	return new InheritedObjectScope(keys.split('|'), proto);
+type objectScope = object;
+type functionScope = (value :string | object | any[]) => string;
+
+export default function Scope (this :objectScope[] | objectScope | any, keys? :string) :objectScope | functionScope {
+	if ( typeof keys==='string' ) {
+		if ( isArray(this) ) { return new InheritedObjectScope(keys.match(KEYS) || EMPTY, InheritedObjectScope.prototype = mix(<objectScope[]>this)); }
+		else if ( this instanceof ObjectScope ) { return new InheritedObjectScope(keys.match(KEYS) || EMPTY, InheritedObjectScope.prototype = <objectScope>this); }
+		else { return new ObjectScope(keys.match(KEYS) || EMPTY); }
+	}
+	else {
+		if ( isArray(this) ) { return FunctionScope(mix(<objectScope[]>this)); }
+		else if ( this instanceof ObjectScope ) { return FunctionScope(create(<objectScope>this)); }
+		else { return FunctionScope(create(null)); }
+	}
 };
 
-function mix (this :void, protos :object[]) :object {
-	var scope :object = create(null);
+function mix (this :void, protos :objectScope[]) :objectScope {
+	var scope :objectScope = create(null);
 	for ( var length :number = protos.length, index = 0; index<length; ++index ) {
-		var proto :object = protos[index];
+		var proto :objectScope = protos[index];
 		for ( var id in proto ) { scope[id] = proto[id]; }
 	}
-	return scope;
-}
-
-function FunctionScope (this: void, cache :object) :Function {
-	function scope (key :string) :string { return cache[key] || ( cache[key] = Identifier() ); }
-	scope._ = function (string :string) { return string.replace(SEARCH, replacer); };
-	function replacer (__key__ :string) :string { return scope(__key__.slice(2, -2)); }
 	return scope;
 }
 
@@ -41,7 +41,7 @@ function ObjectScope (keys :string[]) :void {
 	for ( var index :number = keys.length; index; ) { this[keys[--index]] = Identifier(); }
 }
 
-function InheritedObjectScope (keys :string[], proto :object) :void {
+function InheritedObjectScope (keys :string[], proto :objectScope) :void {
 	this._ = function (string :string) { return string.replace(search, replacer); };
 	for ( var index :number = keys.length; index; ) { this[keys[--index]] = Identifier(); }
 	for ( var key in proto ) { key==='_' || keys.push(key); }
@@ -50,10 +50,48 @@ function InheritedObjectScope (keys :string[], proto :object) :void {
 	InheritedObjectScope.prototype = NULL;
 }
 
+function FunctionScope (this :void, cache :objectScope) :functionScope {
+	function scope (value :string | object | any[]) :string { return scopify(value, _scope); }
+	scope._ = function (string :string) { return string.replace(SEARCH, _replacer); };
+	function _replacer (__key__ :string) :string { return _scope(__key__.slice(2, -2)); }
+	function _scope (key :string) :string { return cache[key] || ( cache[key] = Identifier() ); }
+	return scope;
+}
+
 function Search (keys :string[]) :RegExp {
 	return new RegExp('__'+groupify(keys, false, true)+'__', 'g');
 }
 
-function Replacer (scope :object) {
+function Replacer (scope :objectScope) {
 	return function replacer (__key__ :string) :string { return scope[__key__.slice(2, -2)]; };
+}
+
+function scopify (value :string | object | any[], _scope :(key :string) => string) :string {
+	var keys :string,
+		index :number,
+		key :string;
+	if ( typeof value==='string' ) {
+		if ( ( <string>value ).indexOf(' ')=== -1 ) { return _scope(value); }
+		keys = '';
+		var values :string[] = value.split(' ');
+		for ( index = values.length; index--; ) {
+			key = value[index];
+			if ( key ) { keys = _scope(key)+' '+keys; }
+		}
+		return keys && keys.slice(0, -1);
+	}
+	var keys = '';
+	if ( isArray(value) ) {
+		for ( index = ( <string[]>value ).length; index--; ) {
+			key = scopify(value[index], _scope);
+			if ( key ) { keys = key+' '+keys; }
+		}
+		return keys && keys.slice(0, -1);
+	}
+	else {
+		for ( key in <object>value ) {
+			if ( value[key] ) { keys += ' '+_scope(key); }
+		}
+		return keys && keys.slice(1);
+	}
 }
